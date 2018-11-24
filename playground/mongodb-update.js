336cf482fd3ea3ee0b6a2e6250bfab3afaa5fe78
prakash <you@example.com>
2018-11-24 23:21:04 +0530

    msg

---
+++ playground/mongodb-update.js
@@ -0,0 +1,47 @@
+var {MongoClient, ObjectId} = require('mongodb');  // instead of above statement we can also use this...ES6 feature...
+
+// var obj = new ObjectId();
+//
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/todoapp', (err, client) => {
+
+  if (err)
+  return console.log('db is not connected');
+  console.log('sucessfully connected');
+
+//update....
+
+/*
+1.findOneAndUpdate => find one and update that record..
+*/
+const db = client.db('todoapp');
+
+// db.collection('users').findOneAndUpdate({name:'ravi'},
+//   {
+//     '$set':{
+//   name:'kumar'           //updating the record by using $set operator
+// }
+// },
+// {
+//   returnOriginal:false   //dont return original
+// }).then((res) => {
+//   console.log(res);
+// });
+
+
+//updating the age of a perosn
+
+db.collection('users').findOneAndUpdate({name:'ram'},{
+  '$inc':{
+    age:1
+  }
+}, {
+  returnOriginal:false
+}).then((results) => {
+  console.log(results);
+}, (error) => {
+  console.log(error);
+});
+  client.close();
+});
